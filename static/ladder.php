43d0a9efbcb176fba30ec1244b86b34d8eed3ac3
Oiawesome <Oiawesome99@yahoo.com>
2014-02-15 20:57:00 -0800

    stuffs

---
+++ static/ladder.php
@@ -0,0 +1,54 @@
+<?php
+
+include 'lib/ntbb-ladder.lib.php';
+
+$serverid = 'showdown';
+$formatid = 'OU';
+
+if (@$_REQUEST['format']) $formatid = $_REQUEST['format'];
+if (@$_REQUEST['server']) $serverid = $_REQUEST['server'];
+
+if (!ctype_alnum($formatid)) {
+	die('denied');
+}
+
+if (isset($_REQUEST['testclient'])) {
+	header('Content-Type: text/plain; charset=utf-8');
+}
+
+$ladder = new NTBBLadder($serverid, $formatid);
+?>
+	<table>
+		<tr>
+			<th></th><th>Name</th><th><abbr title="Elo rating">Elo</abbr></th><th><abbr title="user's percentage chance of winning a random battle (aka GLIXARE)">GXE</abbr></th><th><abbr title="Glicko-1 rating system: rating&#177;deviation (provisional if deviation>100)">Glicko-1</abbr></th>
+			<th><abbr title="COIL score (just testing)">MYSTERY <br />RATING A</abbr></th><th><abbr title="ARMS score (just testing)">MYSTERY <br />RATING B</abbr></th>
+		</tr>
+<?php
+
+$toplist = $ladder->getTop();
+
+$i=0;
+
+if (!count($toplist))
+{
+?>
+		<tr>
+			<td colspan="8"><em>No one has played any ranked games yet.</em></td>
+		</tr>
+<?php
+}
+foreach ($toplist as $row)
+{
+	$i++;
+	$N=$row['w']+$row['l']+$row['t'];
+?>
+		<tr<?php /* if (floatval($row['rprd']) > 100) echo ' style="color:#999"'; */ ?>>
+			<td><?php echo $i; ?></td><td><?php echo htmlspecialchars($row['username']); ?></td><td><strong><?php echo round($row['acre']); ?></strong></td><td><?php echo number_format($row['gxe'],1); ?></td>
+			<td><?php echo '<em>'.round($row['rpr']).'<small> &#177; '.round($row['rprd']).'</small></em>'; /* if (floatval($row['rprd']) > 100) echo ' <small>(provisional)</small>'; */ ?></td>
+			<td><?php echo number_format(40*$row['gxe']*pow(2.0,-34.0/$N),1,'.',''); ?></td>
+			<td><?php echo number_format(1000+(2*$row['gxe']-100)*$N/4,1,'.',''); ?></td>
+		</tr>
+<?php
+}
+?>
+	</table>
